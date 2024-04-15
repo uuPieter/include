14219c0bc861fb752ff9f1eaa7721360bf96208f
uuPieter <166505301+uuPieter@users.noreply.github.com>
2024-04-15 14:12:40 +0200

    Add files via upload

---
+++ include/pwhg_pdf.h
@@ -0,0 +1,6 @@
+c -*- Fortran -*-
+
+      integer pdf_ih1,pdf_ih2,pdf_ndns1,pdf_ndns2
+      real * 8 pdf_q2min
+      common/pwhg_pdf/pdf_q2min,pdf_ih1,pdf_ih2,pdf_ndns1,pdf_ndns2
+      save /pwhg_pdf/
